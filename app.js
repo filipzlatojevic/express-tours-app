9c7302f09639ad326aaf3e7c1b70af62f983a029
filipzlatojevic <ficozlatojevic@gmail.com>
2024-03-16 19:50:22 +0100

    feat(all): add project.

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev')); // pokazuje nam u konzoli vreme odziva, status...
+}
+
+app.use(express.json()); // da mozemo da citamo json iz req
+app.use(express.static(`${__dirname}/public`)); // da bismo mogli da prikazemo fajlove iz public
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+// app.route('/api/v1/tours').get(gettAllTours).post(createTour);
+
+// app
+//   .route('/api/v1/tours/:id')
+//   .get(getTour)
+//   .patch(updateTour)
+//   .delete(deleteTour);
+
+// app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+// app
+//   .route('/api/v1/users/:id')
+//   .get(getUser)
+//   .patch(updateUser)
+//   .delete(deleteUser);
+
+module.exports = app;
